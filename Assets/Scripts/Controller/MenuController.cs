79e4a383d63f16d9597bdab3acd0cf9e03140e2e
waloche <waloche@yahoo.com>
2016-02-12 15:25:02 +0100

    Fixed conflicted commit

--- Assets/Scripts/Controller/MenuController.cs
+++ Assets/Scripts/Controller/MenuController.cs
@@ -3,26 +3,27 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
 
+
 /// <summary>
 /// This class manage the Menu view.
 /// </summary>
 public class MenuController : MonoBehaviour
 {
-    private Text introText;
+    private PlayerProfileData profileDataObj = PlayerProfileData.profileData;
     /// <summary>The game state.</summary>
     private GameState state = GameState.LOGO;
-	/// <summary>The logo animator.</summary>
-	public Animator logoAnimator;
-	/// <summary>The play animator.</summary>
-	public Animator playAnimator;
-	/// <summary>The intro animator.</summary>
-	public Animator introAnimator;
+    /// <summary>The logo animator.</summary>
+    public Animator logoAnimator;
+    /// <summary>The play animator.</summary>
+    public Animator playAnimator;
+    /// <summary>The intro animator.</summary>
+    public Animator introAnimator;
     /// <summary>The setup button animator.</summary>
     public Animator setupAnimator;
     /// <summary>The setup button animator.</summary>
     public Animator backAnimator;
 
-
+    private Text introText;
 
     /// <summary>
     /// Called when the play button is cliqued. Show the correct view (intro, then the game).
@@ -39,22 +40,22 @@ public class MenuController : MonoBehaviour
         var difference = System.DateTime.Now.Subtract(profileDataObj.lastConnectionDate).TotalDays;
 
 
-        if (difference>2)
+        if (difference > 2)
         {
-            introText.text= "Content de te revoir !\r\nTu nous as manqué !";
-        } 
+            introText.text = "Content de te revoir !\r\nTu nous as manqué !";
+        }
 
         profileDataObj.lastConnectionDate = System.DateTime.Now;
         PlayerProfileData.profileData.Save();
     }
 
-    public void PlayClicked ()
-	{
-		if (state == GameState.LOGO)
-			ShowIntro ();
-		else if (state == GameState.INTRO)
-			StartGame ();
-	}
+    public void PlayClicked()
+    {
+        if (state == GameState.LOGO)
+            ShowIntro();
+        else if (state == GameState.INTRO)
+            StartGame();
+    }
 
     public void SetupClicked()
     {
@@ -67,29 +68,155 @@ public class MenuController : MonoBehaviour
         Application.LoadLevel("Menu");
     }
 
+    // Add or remove one value in the parameters
+    //nb Breathing High
+    public void nbBreathingsHighDecClicked()
+    {
+        profileDataObj.nbBreathingsHigh -= 1;
+
+        if (profileDataObj.nbBreathingsHigh < 0)
+        {
+            profileDataObj.nbBreathingsHigh = 0;
+        }
+    }
+
+    public void nbBreathingsHighIncClicked()
+    {
+        profileDataObj.nbBreathingsHigh += 1;
+
+        if (profileDataObj.nbBreathingsHigh > 15)
+        {
+            profileDataObj.nbBreathingsHigh = 15;
+        }
+
+    }
+
+    //nbBreathingsMedium
+    public void nbBreathingsMediumDecClicked()
+    {
+        profileDataObj.nbBreathingsMedium -= 1;
+
+        if (profileDataObj.nbBreathingsMedium < 0)
+        {
+            profileDataObj.nbBreathingsMedium = 0;
+        }
+    }
+
+    public void nbBreathingsMediumIncClicked()
+    {
+        profileDataObj.nbBreathingsMedium += 1;
+
+        if (profileDataObj.nbBreathingsMedium > 15)
+        {
+            profileDataObj.nbBreathingsMedium = 15;
+        }
+    }
+
+    //nbBreathingsLow
+    public void nbBreathingsLowDecClicked()
+    {
+        profileDataObj.nbBreathingsLow -= 1;
+
+        if (profileDataObj.nbBreathingsLow < 0)
+        {
+            profileDataObj.nbBreathingsLow = 0;
+        }
+    }
+
+    public void nbBreathingsLowIncClicked()
+    {
+        profileDataObj.nbBreathingsLow += 1;
+
+        if (profileDataObj.nbBreathingsLow > 15)
+        {
+            profileDataObj.nbBreathingsLow = 15;
+        }
+    }
+
+    public void inspirationTimeDecClicked()
+    {
+        profileDataObj.inspirationTime -= .5f;
+
+        if (profileDataObj.inspirationTime < 0)
+        {
+            profileDataObj.inspirationTime = 0;
+        }
+    }
+
+    public void inspirationTimeIncClicked()
+    {
+        profileDataObj.inspirationTime += .5f;
+
+        if (profileDataObj.inspirationTime > 15)
+        {
+            profileDataObj.inspirationTime = 15;
+        }
+    }
+
+    public void holdingBreathTimeDecClicked()
+    {
+        profileDataObj.holdingBreathTime -= .5f;
+
+        if (profileDataObj.holdingBreathTime < 0)
+        {
+            profileDataObj.holdingBreathTime = 0;
+        }
+    }
+
+    public void holdingBreathTimeTimeIncClicked()
+    {
+        profileDataObj.holdingBreathTime += .5f;
+
+        if (profileDataObj.holdingBreathTime > 15)
+        {
+            profileDataObj.holdingBreathTime = 15;
+        }
+    }
+
+    public void expirationMinTimeTimeDecClicked()
+    {
+        profileDataObj.expirationMinTime -= .5f;
+
+        if (profileDataObj.expirationMinTime < 0)
+        {
+            profileDataObj.expirationMinTime = 0;
+        }
+    }
+
+    public void expirationMinTimeTimeIncClicked()
+    {
+        profileDataObj.expirationMinTime += .5f;
+
+        if (profileDataObj.expirationMinTime > 15)
+        {
+            profileDataObj.expirationMinTime = 15;
+        }
+    }
+
+
     /// <summary>
     /// Shows the intro text (doing all the animations).
     /// </summary>
     /// 
 
-    private void ShowIntro ()
-	{
-		state = GameState.INTRO;
-		
-		logoAnimator.SetBool ("visible", false);
-		introAnimator.SetBool ("visible", true);
-	}
+    private void ShowIntro()
+    {
+        state = GameState.INTRO;
 
-	/// <summary>
-	/// Load the game scene.
-	/// </summary>
-	private void StartGame ()
-	{
-		playAnimator.SetBool ("visible", false);		
-		introAnimator.SetBool ("visible", false);
-        setupAnimator.SetBool ("visible", false);
+        logoAnimator.SetBool("visible", false);
+        introAnimator.SetBool("visible", true);
+    }
 
-        Application.LoadLevel ("GreeceGenerated");
-	}
-	
+    /// <summary>
+    /// Load the game scene.
+    /// </summary>
+    private void StartGame()
+    {
+        playAnimator.SetBool("visible", false);
+        introAnimator.SetBool("visible", false);
+        setupAnimator.SetBool("visible", false);
+
+        Application.LoadLevel("GreeceGenerated");
+    }
+
 }
